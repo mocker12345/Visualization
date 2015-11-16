b88e0b5bd1d97b167fc5c23f577b32e5c8dc3a52
rancongjie <rancongjie@126.com>
2015-11-16 19:43:39 +0800

    解决 连续点击同一音乐 重复播放，快速点击不同音乐会同时播放的问题
    解决 连续点击同一音乐 重复播放，快速点击不同音乐会同时播放的问题

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,15 +20,28 @@ window.onload = function () {
   var audio = new window.AudioContext();
   var gainNode = audio[audio.createGain ? 'createGain' : 'createGainNode']();
   gainNode.connect(audio.destination);
+  var source = null;
+  var count = 0;
   function load(url) {
+    //防止连续点击造成同时播放
+    var n = ++count;
+    //如果source已经有内容,则停止掉当前播放
+    source && source[source.stop ? 'stop' : 'nodeOff'](0);
+    //终止上一次请求
+    xhr.abort();
     xhr.open('GET', url);
     xhr.responseType = 'arraybuffer';
     xhr.onload = function () {
+      //如果 n 不等于 count 则说明在还没有解码之前就点击了下一首歌曲
+      if (n !== count)return;
       audio.decodeAudioData(xhr.response, function (buffer) {
+        if (n !== count)return;
         var bufferSource = audio.createBufferSource();
         bufferSource.buffer = buffer;
         bufferSource.connect(gainNode);
-        bufferSource[bufferSource.start ? "start" : "noteOn"]();
+        bufferSource[bufferSource.start ? "start" : "noteOn"](0);
+        //把bufferSource 赋值给 source,判断是否正在播放
+        source = bufferSource;
       }, function (err) {
         console.log(err);
       });
@@ -36,11 +49,12 @@ window.onload = function () {
     xhr.send();
   }
 
-  function adJust(persent) {
-    gainNode.gain.value = persent * persent;
+  function adJust(percent) {
+    gainNode.gain.value = percent * percent;
   }
 
   $(".volume")[0].addEventListener('change', function () {
     adJust(this.value / this.max)
   });
+  gainNode.gain.value = $(".volume")[0].value / 100;
 };
